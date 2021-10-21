05a4d9cf911b3f890ca9f321e3fe81e4abdfd039
Huijo <ccomkhj@gmail.com>
2021-10-21 20:33:14 +0200

    config update

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_480x480_1k_LeafDataset.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_480x480_1k_LeafDataset.py
@@ -5,6 +5,12 @@ _base_ = [
 ]
 model = dict(
     decode_head=dict(num_classes=2),
-    auxiliary_head=dict(num_classes=2),
+    auxiliary_head=dict(num_classes=2, align_corners=True,
+                    loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0,
+            # DeepLab used this class weight for cityscapes
+            class_weight=[1,100])),
     test_cfg=dict(mode='slide', crop_size=(480, 480), stride=(320, 320)))
-optimizer = dict(type='SGD', lr=0.004, momentum=0.9, weight_decay=0.0001)
+# optimizer = dict(type='SGD', lr=0.004, momentum=0.9, weight_decay=0.0005)
+
+
