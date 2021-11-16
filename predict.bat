81d47a05c0481a84edd432bb424768e0af34031a
Huijo <ccomkhj@gmail.com>
2021-11-16 11:05:39 +0100

    Delete Unnecessary Files

--- predict.bat
+++
@@ -1,3 +0,0 @@
-python Hexafarm/predict.py work_dirs\segformer_mit-b0_512x512_10k_LeafDataset\segformer_mit-b0_512x512_10k_LeafDataset.py ^
-work_dirs\segformer_mit-b0_512x512_10k_LeafDataset\iter_10000.pth ^
-demo\image-1550434545.jpg --output output
